ef89e139a6208d6a0a1dd9ea32c638ea8fb09525
Mark24 <mark.zhangyoung@qq.com>
2022-08-09 22:04:40 +0800

    update: tui widget::Text

--- lib/mchat/tui.rb
+++ lib/mchat/tui.rb
@@ -1,7 +1,5 @@
 require 'curses'
 
-
-
 module Tui
   module FontStyle
     # 值来源
@@ -45,6 +43,10 @@ module Tui
       Curses.start_color
     end
 
+    def init_window
+      Curses::Window.new(Curses.lines, Curses.cols, 0, 0)
+    end
+
     def refresh
       Curses.refresh
     end
@@ -56,144 +58,70 @@ module Tui
     end
   end
 
-  class Box
-    # 模仿盒模型
-    # * 具有 对齐
-    # * 具有颜色
+  class Widget
+    class Text
+      ConfigEnum = Struct.new(:color, :bg_color, :font_style)
+      DefaultConfig = ConfigEnum.new('white', 'black', 'normal')
 
-    # 定位是放在 window里面一行
-    # 类似 block\inline-block 
-    # 简化每次伴随attr设置
-
-    def initialize(window = nil, content = nil)
-      @window = window
-      @content = content
-      @attr = nil
-    end
-
-    def mount(window)
-      @window = window
-    end
-
-    # def bak_render(row,col)
-    #   result = "#{content}" + " " * (@window_width - content.length)
-
-
-    #   @window.setpos(row, col)
-    #   # Curses.init_pair(Curses::COLOR_BLUE, Curses::COLOR_BLUE, Curses::COLOR_GREEN)
-    #   @window.attron(Curses.color_pair(Curses::COLOR_BLUE)) do
-    #     @window.addstr(result)
-    #   end
-    # end
-
-
-    def styles(opt = {})
-      @color = opt[:color] || 'white'
-      @bg_color = opt[:bg_color] || 'black'
-      @font_style = opt[:font_style] || 'normal'
-
-      @display = opt[:display] || 'block' # block / inline
-      @text_align = opt[:text_align] || 'left' # left center right
-
-      @width = opt[:width] || nil # || @window.cols
-      @height = opt[:height] || nil #|| @window.lines
-      
-      if @display == 'block' && @width == nil
-        @width = @window.maxx
+      def initialize(window = nil, content = nil)
+        @window = window
+        @content = content
+        
+        @color = DefaultConfig.color
+        @bg_color = DefaultConfig.bg_color
+        @font_style = DefaultConfig.font_style
       end
 
-      if @display == 'inline' && @width == nil
-        @width = @content.length
+      def mount(window)
+        @window = window
       end
-    end
 
-    def render
-      # Setting attributes
 
-      ## Origin Curses Example:
-      # curses 内部使用了二进制比特位置来代表字体颜色信息
-      # ```ruby
-      # Curses.init_pair(Curses::COLOR_BLUE, Curses::COLOR_BLUE, Curses::COLOR_GREEN)
-      # Curses.init_pair(Curses::COLOR_RED, Curses::COLOR_RED, Curses::COLOR_WHITE)
+      def styles(opt = {})
+        @color = opt[:color]
+        @bg_color = opt[:bg_color]
+        @font_style = opt[:font_style]
+      end
 
-      # Curses.attron(Curses.color_pair(Curses::COLOR_RED)) do
-      #   Curses.addstr("example")
-      # end
-      # ```
 
-      font_color = ::Module.const_get("Tui::Color::#{@color.to_s.upcase}")
-      bg_color = ::Module.const_get("Tui::Color::#{@bg_color.to_s.upcase}")
-      font_style = ::Module.const_get("Tui::FontStyle::#{@font_style.to_s.upcase}")
-      Curses.init_pair(font_color, font_color, bg_color)
-      
-      
-      # Setting composing
-      
-      result = nil
+      def render
+        # Setting attributes
 
-      ## text_align
-      if @content.length < @width
-        if @text_align == 'left'
-          result = @content + " " * (@width - @content.length)
+        ## Origin Curses Example:
+        # curses 内部使用了二进制比特位置来代表字体颜色信息
+        # ```ruby
+        # Curses.init_pair(Curses::COLOR_BLUE, Curses::COLOR_BLUE, Curses::COLOR_GREEN)
+        # Curses.init_pair(Curses::COLOR_RED, Curses::COLOR_RED, Curses::COLOR_WHITE)
+
+        # Curses.attron(Curses.color_pair(Curses::COLOR_RED)) do
+        #   Curses.addstr("example")
+        # end
+        # ```
+
+        font_color = ::Module.const_get("Tui::Color::#{@color.to_s.upcase}")
+        bg_color = ::Module.const_get("Tui::Color::#{@bg_color.to_s.upcase}")
+        font_style = ::Module.const_get("Tui::FontStyle::#{@font_style.to_s.upcase}")
+        Curses.init_pair(font_color, font_color, bg_color)
+        
+
+        # Render
+        @window.attron(Curses.color_pair(font_color)|font_style) do
+          @window.addstr(@content)
         end
-        if @text_align == 'right'
-          result = " " * (@width - @content.length) + @content 
-        end
-
-        if @text_align == 'center'
-          left_gap = (@width - @content.length)/2 
-          right_gap = @width - @content.length - left_gap
-          result = " " * left_gap + @content + " " * right_gap
-        end
-      end
-
-      if @content.length == @width
-        result = @content
-      end
-
-      if @content.length > @width
-        result = @content[0 .. @width-1 - 3] + '...'
-      end
-
-
-
-      ## display block
-      current_y = @window.cury
-      current_x = @window.curx
-      
-      if @display == 'block'
-        current_y = current_y + 1
-      end
-
-    
-      
-
-      # Render
-      @window.setpos(current_y, current_x)
-      @window.attron(Curses.color_pair(font_color)|font_style) do
-        @window.addstr(result)
       end
     end
   end
 end
 
 Tui.init_screen
-window = Curses::Window.new(Curses.lines, Curses.cols, 0, 0)
+window = Tui.init_window
 
-b = Tui::Box.new(window, "Hello World")
-b.styles({color: 'green', bg_color: 'green', font_style: 'bold', display: 'block'})
+b = Tui::Widget::Text.new(window, "Hello World")
+b.styles({color: 'green', bg_color: 'green', font_style: 'bold'})
 b.render
 
-c = Tui::Box.new(window, "example")
-c.styles({color: 'red', bg_color: 'white', font_style: 'blink', display: 'inline'})
-c.render
-
-div = Tui::Box.new(window, " div 100% ")
-div.styles({color: 'blue', bg_color: 'green', font_style: 'blink', display: 'block'})
-div.render
-
-d = Tui::Box.new(window, "width is10")
-d.styles({color: 'blue', bg_color: 'green', font_style: 'blink', display: 'block', width: 5})
+d = Tui::Widget::Text.new(window, "width is10")
+d.styles({color: 'blue', bg_color: 'red', font_style: 'blink'})
 d.render
 
 
