4a02c964427031ec563afcaf1b146ff7056c1bc9
Mark24 <mark.zhangyoung@qq.com>
2022-08-12 23:13:16 +0800

    refactor: message use style

--- lib/mchat/comps/message.rb
+++ lib/mchat/comps/message.rb
@@ -1,5 +1,3 @@
-require "rainbow"
-
 module Mchat
   class Message
     def initialize(message, format = :std)
@@ -10,7 +8,7 @@ module Mchat
     def display
       if @format == :std
         # Time format https://devdocs.io/ruby~3/datetime#method-i-strftime
-        "[#{Rainbow(Time.at(@message["timestamp"].to_i).strftime("%H:%M:%S")).blue}] #{Rainbow(@message["user_name"]).bold.green}: #{@message["content"].strip}\n"
+        "[#{Time.at(@message["timestamp"].to_i).strftime("%H:%M:%S").style.blue}] #{@message["user_name"].style.bold.green}: #{@message["content"].strip}\n"
       end
     end
   end
