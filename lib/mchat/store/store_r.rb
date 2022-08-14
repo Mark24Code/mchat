f8545481f71d7b5867d5eb2e50d0d9db9bb7e905
Mark24 <mark.zhangyoung@qq.com>
2022-08-14 20:22:16 +0800

    refactor: store reader&writer

--- lib/mchat/store/store_r.rb
+++ lib/mchat/store/store_r.rb
@@ -2,23 +2,14 @@ require_relative './store'
 require 'thread'
 
 reader = Object.new
+reader.extend Mchat::Store
 reader.extend Mchat::Store::Config
 
 reader.instance_eval {
   store = get_store
-  thx = Thread.new {
-    loop do
-      store.transaction do
-        messages = store[:messages]
-        messages.each do |m|
-          puts m
-        end
-        store[:messages_history] += messages
-        store[:messages] = []
-      end
-      sleep 2
+  message_loop_reader { |messages|
+    messages.each do |m|
+      puts m
     end
   }
-
-  thx.join
 }
--- lib/mchat/store/store_w.rb
+++ lib/mchat/store/store_w.rb
@@ -1,27 +1,12 @@
 require_relative './store'
 require 'thread'
 
-o = Object.new
-o.extend Mchat::Store::Config
+writer = Object.new
+writer.extend Mchat::Store
+writer.extend Mchat::Store::Config
 
-o.instance_eval {
+writer.instance_eval {
   store = get_store
 
-  thx = Thread.new {
-    store.transaction do
-      store[:messages_history] ||= Array.new
-      store[:messages] ||= Array.new
-    end
-
-    loop do
-      store.transaction do
-        t = Time.now.to_i
-        puts t
-        store[:messages] << t
-      end
-      sleep 2
-    end
-  }
-
-  thx.join
+  message_writer("hello world")
 }
